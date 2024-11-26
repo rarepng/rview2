66fc3b2a7a4b45ee071ac210237b1ec1ee3524e1
anwar <inksomnia6661@gmail.com>
2024-11-26 22:37:11 +0200

    directories

--- src/controller.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class controller {
-public:
-	void init();
-};
--- src/mtx.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR
-#include <mutex>
-#include <memory>
-
-
-static std::shared_ptr<std::mutex>  mtx;
