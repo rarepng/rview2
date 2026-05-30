e9ce3d2d207c9502fa3695395df65fb27f71bcab
r <inksomnia6661@gmail.com>
2026-05-30 19:04:06 +0200

    cleanup & deployment

--- include/timer.hpp
+++ include/timer.hpp
@@ -3,8 +3,18 @@
 
 class timer {
 public:
-	void start();
-	float stop();
+	inline void start() {
+		if (mstate) return;
+		mstate = true;
+		mstart = std::chrono::steady_clock::now();
+	}
+
+	inline float stop() {
+		if (!mstate) return 0.0f;
+		mstate = false;
+		return std::chrono::duration_cast<std::chrono::milliseconds>(
+			std::chrono::steady_clock::now() - mstart).count() / 1000.0f;
+	}
 
 private:
 	bool mstate = false;
